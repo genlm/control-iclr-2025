4032bb08fa44efc72c2357780f0512ead1107068
benlebrun <benlebrun1@gmail.com>
2025-05-03 14:41:10 -0400

    lm name

--- control_iclr/molecular_synthesis/run_all.sh
+++ control_iclr/molecular_synthesis/run_all.sh
@@ -8,51 +8,46 @@ GPU_LM_ARGS="{\"engine_opts\":{\"max_model_len\":7760,\"rope_scaling\":{\"rope_t
 MODEL_NAME="meta-llama/Meta-Llama-3.1-8B"
 
 # Base language model
-python cli.py --model-type base --smiles-file $SMILES_FILE --lm-args $GPU_LM_ARGS --output-dir $RESULTS_DIR/base_lm
+python cli.py --lm-name $MODEL_NAME --model-type base --smiles-file $SMILES_FILE --lm-args $GPU_LM_ARGS --output-dir $RESULTS_DIR/base_lm
 
 # Locally-constrained decoding
-python cli.py --model-type lcd --smiles-file $SMILES_FILE --lm-args $GPU_LM_ARGS --output-dir $RESULTS_DIR/lcd
+python cli.py --lm-name $MODEL_NAME --model-type lcd --smiles-file $SMILES_FILE --lm-args $GPU_LM_ARGS --output-dir $RESULTS_DIR/lcd --n-replicates 10
 
 # Grammar-only Importance Sampling
-python cli.py --model-type grammar-only-is \
+python cli.py --lm-name $MODEL_NAME --model-type grammar-only-is \
     --smiles-file $SMILES_FILE \
     --lm-args $GPU_LM_ARGS \
-    --model-name $MODEL_NAME \
     --n-particles 10 \
     --output-dir $RESULTS_DIR/grammar_only_is
 
 # Grammar-only SMC
-python cli.py --model-type grammar-only-smc \
+python cli.py --lm-name $MODEL_NAME --model-type grammar-only-smc \
     --smiles-file $SMILES_FILE \
     --lm-args $GPU_LM_ARGS \
-    --model-name $MODEL_NAME \
     --n-particles 10 \
     --ess-threshold 0.9 \
     --resampling-method stratified \
     --output-dir $RESULTS_DIR/grammar_only_smc
 
 # Sample Rerank
-python cli.py --model-type sample-rerank \
+python cli.py --lm-name $MODEL_NAME --model-type sample-rerank \
     --smiles-file $SMILES_FILE \
     --n-particles 10 \
     --lm-args $GPU_LM_ARGS \
-    --model-name $MODEL_NAME \
     --output-dir $RESULTS_DIR/sample_rerank
 
 # Full Importance Sampling
-python cli.py --model-type full-is \
+python cli.py --lm-name $MODEL_NAME --model-type full-is \
     --smiles-file $SMILES_FILE \
     --n-particles 10 \
     --lm-args $GPU_LM_ARGS \
-    --model-name $MODEL_NAME \
     --output-dir $RESULTS_DIR/full_is
 
 # Full SMC
-python cli.py --model-type full-smc \
+python cli.py --lm-name $MODEL_NAME --model-type full-smc \
     --smiles-file $SMILES_FILE \
     --n-particles 10 \
     --ess-threshold 0.9 \
     --resampling-method stratified \
     --lm-args $GPU_LM_ARGS \
-    --model-name $MODEL_NAME \
     --output-dir $RESULTS_DIR/full_smc
